ed47e6e67678128528d1c330f6a7c7ba91f37427
寒夕 <318745470@qq.com>
2023-11-27 07:32:41 +0800

    后端工程搭建，实现登录接口

---
+++ spzx-manager/src/main/java/com/hanxi/spzx/manager/ManagerApplication.java
@@ -0,0 +1,16 @@
+package com.hanxi.spzx.manager;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//Knife4j不在同一个模块，需要扫包
+//@ComponentScan(basePackages = {"org"})
+//@MapperScan(basePackages = ("com.hanxi.spzx.manager"))
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class,args);
+    }
+}
---
+++ spzx-manager/src/main/java/com/hanxi/spzx/manager/controller/IndexController.java
@@ -0,0 +1,42 @@
+package com.hanxi.spzx.manager.controller;
+
+import com.hanxi.spzx.manager.service.SysUserService;
+import com.hanxi.spzx.manager.service.ValidateCodeService;
+import com.hanxi.spzx.model.dto.system.LoginDto;
+import com.hanxi.spzx.model.vo.common.Result;
+import com.hanxi.spzx.model.vo.common.ResultCodeEnum;
+import com.hanxi.spzx.model.vo.system.LoginVo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "用户接口")
+@RestController
+@RequestMapping(value = "/admin/system/index")
+public class IndexController {
+
+    @Autowired
+    private SysUserService sysUserService ;
+    @Autowired
+    private ValidateCodeService validateCodeService;
+
+    @Operation(summary = "登录接口")
+    @PostMapping(value = "/login")
+    public Result<LoginVo> login(@RequestBody LoginDto loginDto) {
+        LoginVo loginVo = sysUserService.login(loginDto) ;
+        return Result.build(loginVo , ResultCodeEnum.SUCCESS) ;
+    }
+
+
+//    @Operation(summary = "验证码接口")
+//    @GetMapping(value = "/generateValidateCode")
+//    public Result<ValidateCodeVo> generateValidateCode() {
+//        ValidateCodeVo validateCodeVo = validateCodeService.generateValidateCode();
+//        return Result.build(validateCodeVo , ResultCodeEnum.SUCCESS) ;
+//    }
+
+}
