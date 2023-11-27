a3ec3c972b08940fe39e2868b1792af05cae2a16
寒夕 <318745470@qq.com>
2023-11-27 13:54:09 +0800

    获取用户信息，退出登录

--- spzx-manager/src/main/java/com/hanxi/spzx/manager/controller/IndexController.java
+++ spzx-manager/src/main/java/com/hanxi/spzx/manager/controller/IndexController.java
@@ -3,12 +3,14 @@ package com.hanxi.spzx.manager.controller;
 import com.hanxi.spzx.manager.service.SysUserService;
 import com.hanxi.spzx.manager.service.ValidateCodeService;
 import com.hanxi.spzx.model.dto.system.LoginDto;
+import com.hanxi.spzx.model.entity.system.SysUser;
 import com.hanxi.spzx.model.vo.common.Result;
 import com.hanxi.spzx.model.vo.common.ResultCodeEnum;
 import com.hanxi.spzx.model.vo.system.LoginVo;
 import com.hanxi.spzx.model.vo.system.ValidateCodeVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +23,20 @@ public class IndexController {
     private SysUserService sysUserService ;
     @Autowired
     private ValidateCodeService validateCodeService;
+
+    @GetMapping(value = "/logout")
+    public Result logout(@RequestHeader(value = "Authorization") String token) {
+        token = StringUtils.substring(token,"Bearer ".length());
+        sysUserService.logout(token) ;
+        return Result.build(null , ResultCodeEnum.SUCCESS) ;
+    }
+
+    @GetMapping(value = "/getUserInfo")
+    public Result<SysUser> getUserInfo(@RequestHeader(name = "Authorization") String token) {
+        token = StringUtils.substring(token,"Bearer ".length());
+        SysUser sysUser = sysUserService.getUserInfo(token) ;
+        return Result.build(sysUser , ResultCodeEnum.SUCCESS) ;
+    }
 
     @Operation(summary = "登录接口")
     @PostMapping(value = "/login")
