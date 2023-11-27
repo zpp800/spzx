691c3b7e1e01390bc35e49b9898bd7fdfa4c610e
寒夕 <318745470@qq.com>
2023-11-27 08:51:51 +0800

    自定义异常和全局异常处理

--- spzx-manager/src/main/java/com/hanxi/spzx/manager/ManagerApplication.java
+++ spzx-manager/src/main/java/com/hanxi/spzx/manager/ManagerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 //Knife4j不在同一个模块，需要扫包
 //@ComponentScan(basePackages = {"org"})
-//@MapperScan(basePackages = ("com.hanxi.spzx.manager"))
+@ComponentScan (basePackages = ("com.hanxi.spzx"))
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class,args);
