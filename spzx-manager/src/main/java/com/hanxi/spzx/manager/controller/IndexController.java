21ca7a29b9f7d7d761c00ba415ce2a14d6449162
寒夕 <318745470@qq.com>
2023-11-27 20:36:58 +0800

    登录拦截校验代码优化

--- spzx-manager/src/main/java/com/hanxi/spzx/manager/controller/IndexController.java
+++ spzx-manager/src/main/java/com/hanxi/spzx/manager/controller/IndexController.java
@@ -1,15 +1,16 @@
 package com.hanxi.spzx.manager.controller;
 
+import com.hanxi.spzc.utils.AuthContextUtil;
 import com.hanxi.spzx.manager.service.SysUserService;
 import com.hanxi.spzx.manager.service.ValidateCodeService;
 import com.hanxi.spzx.model.dto.system.LoginDto;
-import com.hanxi.spzx.model.entity.system.SysUser;
 import com.hanxi.spzx.model.vo.common.Result;
 import com.hanxi.spzx.model.vo.common.ResultCodeEnum;
 import com.hanxi.spzx.model.vo.system.LoginVo;
 import com.hanxi.spzx.model.vo.system.ValidateCodeVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +18,11 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "用户接口")
 @RestController
 @RequestMapping(value = "/admin/system/index")
+@RequiredArgsConstructor
 public class IndexController {
 
-    @Autowired
-    private SysUserService sysUserService ;
-    @Autowired
-    private ValidateCodeService validateCodeService;
-
+    private final SysUserService sysUserService ;
+    private final ValidateCodeService validateCodeService;
     @GetMapping(value = "/logout")
     public Result logout(@RequestHeader(value = "Authorization") String token) {
         token = StringUtils.substring(token,"Bearer ".length());
@@ -32,11 +31,15 @@ public class IndexController {
     }
 
     @GetMapping(value = "/getUserInfo")
-    public Result<SysUser> getUserInfo(@RequestHeader(name = "Authorization") String token) {
-        token = StringUtils.substring(token,"Bearer ".length());
-        SysUser sysUser = sysUserService.getUserInfo(token) ;
-        return Result.build(sysUser , ResultCodeEnum.SUCCESS) ;
+    public Result getUserInfo() {
+        return Result.build(AuthContextUtil.get()  , ResultCodeEnum.SUCCESS) ;
     }
+//    @GetMapping(value = "/getUserInfo")
+//    public Result<SysUser> getUserInfo(@RequestHeader(name = "Authorization") String token) {
+//        token = StringUtils.substring(token,"Bearer ".length());
+//        SysUser sysUser = sysUserService.getUserInfo(token) ;
+//        return Result.build(sysUser , ResultCodeEnum.SUCCESS) ;
+//    }
 
     @Operation(summary = "登录接口")
     @PostMapping(value = "/login")
